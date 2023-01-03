c932d7439c8bbe530dc88edaa0078c704e66c497
Kevin Childs <kchilds2020@gmail.com>
2023-01-03 13:12:12 -0600

    first commit

---
+++ src/main/java/CharComparison.java
@@ -0,0 +1,16 @@
+public class CharComparison {
+    /**
+     * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
+     * That means its position in a dictionary, eg:
+     * cat < dog < mouse
+     * cat < car < care
+     * You will need to compare an index of both arrays against each other as you iterate with a for loop.
+     *
+     * @param a an array of char.
+     * @param b an array of char.
+     * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
+     */
+    public int compare(char[] a, char[] b){
+        return 0;
+    }
+}
